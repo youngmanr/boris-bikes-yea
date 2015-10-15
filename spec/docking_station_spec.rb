7a71c81943261896a94f4bbfee231e54e39af05e
Robert Youngman <rob_youngman@yahoo.com>
2015-10-15 15:59:31 +0100

    Chapter 20

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,20 +1,23 @@
 require 'docking_station'
 
 describe DockingStation do
+  let(:bike) { double :bike }
 
   it { is_expected.to respond_to :release_bike }
 
-  describe '#release_bike' do
-    it 'releases a bike' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.release_bike).to eq bike
-    end
-    it 'raises an error when there are not working bikes available' do
-      bike = Bike.new
-      subject.dock(bike,false)
-      expect {subject.release_bike}.to raise_error "No bikes available"
-    end
+  it 'releases a bike' do
+    allow(bike).to receive(:working=).with(true).and_return(true)
+    allow(bike).to receive(:working?).and_return(true)
+    subject.dock bike
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
+  it 'raises an error when there are not working bikes available' do
+    allow(bike).to receive(:working=).with(false).and_return(false)
+    allow(bike).to receive(:working?).and_return(false)
+    subject.dock bike, false
+    expect { subject.release_bike }.to raise_error "No bikes available"
   end
 
   it { is_expected.to respond_to(:dock).with(1) }
@@ -25,18 +28,11 @@ describe DockingStation do
     expect { subject.release_bike }.to raise_error "No bikes available"
   end
 
-  describe 'initialization' do
-  subject { DockingStation.new }
-  let(:bike) { Bike.new }
   it 'defaults capacity' do
+    allow(bike).to receive(:working=).with(true).and_return(true)
     described_class::DEFAULT_CAPACITY.times do
-      subject.dock(bike)
+      subject.dock bike
     end
-    expect{ subject.dock(bike) }.to raise_error 'Docking station full'
+    expect{ subject.dock bike }.to raise_error 'Docking station full'
   end
-end
-
-
-
-
 end
